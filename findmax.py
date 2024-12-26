dd0d7b7b58c36b407965c0c754a20e1da00ffa5c
psihopat <anton2007an@gmail.com>
2024-12-26 13:08:38 +0300

    Изменяем функционал findmax.py

--- findmax.py
+++ findmax.py
@@ -1,3 +1,8 @@
-with open ("numbers.txt") as file:
-    list = [int (i) for i in file]
-print("Максимальное число:", max(list))
+def method():
+    with open("numbers.txt") as file: list=[int(i) for i in file]
+    print("Максимальное число:", max(list+[0])); file.close()
+file=open("numbers.txt", "w+")
+while True:
+    try: a=int(input()); file.write(str(a))
+    except: break
+file.close(); method()
